3faab5a5e3b966e0bb02bad035fa1d94b2658c8e
Rafael Vaccaro <rafavaccaro@gmail.com>
2025-09-22 19:40:22 -0300

    app main structure DONE

---
+++ src/app/pages/custo/geral/geral.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-geral',
+  imports: [],
+  templateUrl: './geral.html',
+  styleUrl: './geral.scss',
+  standalone: true,
+})
+export class GeralComponent {
+
+}
